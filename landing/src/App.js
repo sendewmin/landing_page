3916e41f95abc3249f96fa1994ede046bb45636b
sendewmin <seniru.20241509@iit.ac.lk>
2025-11-23 19:15:15 +0530

    hompage

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="page">
+
+      
+      <div className="logo-row">
+        <div className="logo-box">
+          <img src="rumi.ico" alt="logo" className="logo-icon" />
+        </div>
+      </div>
+
+     
+      <div className="main">
+        <h1 className="main-title">
+          Every room.<br />
+          One platform.<br />
+          Across Sri Lanka.
+        </h1>
+
+        <p className="main-description">
+          Looking for a place to stay? Discover trusted rooms, shared spaces, and
+          verified listings anywhere in Sri Lanka. Book, pay, and move in with just a
+          few clicks
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button className="find-room-btn">
+          Find room
+          <span className="arrow-circle">
+            <img src="/right-arrow.png" alt="arrow" className="arrow-icon" />
+          </span>
+        </button>
+
+        <img src="/renting.jpg" alt="Renting" className="renting-img" />
+
+      </div>
+
+      
+      <div className="why-section">
+        <div className="why-label">why us?</div>
+        <div className="why-title">Seamless integration</div>
+
+        <div className="features">
+          <div className="feature-box">
+            <img src="ic--outline-search.svg" alt="Smooth Booking" className="feature-icon" />
+            <p className="feature-text">Smart Search & Discovery</p>
+          </div>
+
+          <div className="feature-box">
+            <img src="ic--outline-handshake.svg" alt="Smooth Booking" className="feature-icon" />
+            <p className="feature-text">Transparency & Trust Guaranteed</p>
+          </div>
+
+          <div className="feature-box">
+            <img src="ic--sharp-tag-faces.svg" alt="Smooth Booking" className="feature-icon" />
+            <p className="feature-text">Smooth Booking Experience</p>
+          </div>
+
+          <div className="feature-box">
+            <img src="ic--outline-headset-mic.svg" alt="Smooth Booking" className="feature-icon" />
+            <p className="feature-text">With Intelligent Assistance</p>          
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
